d963704ac48844cfcd948b93c2b4d1815b869a12
RhazzXIX <jm21tonio@gmail.com>
2023-04-02 17:55:37 +0800

    Add App component test and html structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import './styles/App.css';
+import "./styles/App.css";
+import { useState, useEffect } from "react";
+import Header from "./components/Header";
+import gitHub from "./images/github.png";
+import BG from "./images/shoppingBG.webp";
 
 function App() {
+  const [cartItems, setCartItems] = useState([]);
   return (
-    <div className="App">
+    <div id="App">
+      <Header itemsAmount={cartItems.length} />
+      <main>
+        <h1>Hoarder Digital</h1>
+      </main>
+      <footer>
+        <a href="https://github.com/RhazzXIX">
+          <img id="gitHub" src={gitHub} alt="GitHub Profile" />
+        </a>
+      </footer>
     </div>
   );
 }

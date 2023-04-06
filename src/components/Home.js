cbd0d5f8dec79d2d876fe0f380f1881179cbf098
RhazzXIX <jm21tonio@gmail.com>
2023-04-06 14:01:15 +0800

    Add photo credit, update test and style

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ const Home = () => {
   return (
     <main id="home">
       <h1>Hoarder Digital</h1>
+      <p>Photo by: <a href="https://www.pexels.com/@n-voitkevich/">Nataliya Vaitkevich</a></p>
     </main>
   );
 };
--- src/tests/Home.test.js
+++ src/tests/Home.test.js
@@ -1,7 +1,7 @@
 import { render } from "@testing-library/react";
 import Home from "../components/Home";
 
-test("renders learn react link", () => {
+test("renders the home component", () => {
   const { container } = render(<Home />);
   expect(container).toMatchSnapshot();
 });
